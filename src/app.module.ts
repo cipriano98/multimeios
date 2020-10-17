3cbbae0f01b87d8c74cee83e2eea605e247ffef7
Cipriano <natancipriano98@gmail.com>
2020-10-17 20:12:06 -0300

    Ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { PrismaService } from './prisma/prisma.service';
-import { AuthController } from './auth/auth.controller';
 import { UserModule } from './user/user.module';
-import { EmployeeController } from './employee/employee.controller';
-import { EmployeeService } from './employee/employee.service';
 import { EmployeeModule } from './employee/employee.module';
 
 @Global()
