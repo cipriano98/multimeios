1dc94b3b42787689fe34e31042537148c58aa394
Cipriano <natancipriano98@gmail.com>
2020-10-19 06:39:59 -0300

    Add employee

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,76 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, Post, Render, Res } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+import bcrypt = require('bcrypt')
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+    constructor(
+        private service: EmployeeService
+    ) { }
+
+    @Get()
+    @Render('pages/employee/list')
+    async employees() {
+        const getEmployees = await this.service.getMany()
+        if (getEmployees)
+            return {
+                title: 'Funcionários',
+                Employees: getEmployees
+            };
+        throw new HttpException('Não há dados', HttpStatus.NO_CONTENT)
+    }
+
+    @Post('/profile')
+    @HttpCode(200)
+    @Render('pages/employee/profile')
+    async profile(@Body('id') id) {
+        const employee = await this.service.getOne(id)
+        if (employee)
+            return {
+                title: 'Perfil',
+                Employee: employee
+            };
+        throw new HttpException('O funcionário com este id não existe', HttpStatus.NOT_FOUND)
+    }
+
+    @Get('/add')
+    @Render('pages/employee/create')
+    async add() {
+        return {
+            title: 'Novo Funcionário',
+        };
+    }
+
+    @Post('/')
+    @Render('pages/employee/list')
+    async createEmployee(@Res() res, @Body() data) {
+        data.secret = bcrypt.hashSync(data.secret, 10);
+        const newEmployee = await this.service.create(data)
+
+        if (newEmployee) {
+            res.status(HttpStatus.PERMANENT_REDIRECT).redirect('/employee')
+        }
+        return {
+            Employees: newEmployee
+        };
+    }
+
+    @Post('/delete')
+    @HttpCode(200)
+    async deleteEmployee(@Res() res, @Body('id') id) {
+        const deleteEmployee = await this.service.delete(id)
+
+        if (deleteEmployee) {
+            return res.status(HttpStatus.PERMANENT_REDIRECT).redirect('/employee')
+        }
+    }
+    @Post('/alter/:id')
+    @HttpCode(200)
+    async alterEmployee(@Res() res, @Body() data, @Param('id') id) {
+        const altertEmployee = await this.service.update(data, id)
+
+        if (altertEmployee) {
+            res.status(HttpStatus.PERMANENT_REDIRECT).redirect('/employee')
+        }
+    }
+
+}
--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthController } from 'src/auth/auth.controller';
 
 @Module({
     imports: [],
-    controllers: [EmployeeController],
+    controllers: [
+        EmployeeController,
+        AuthController
+    ],
     providers: [
         EmployeeService,
         PrismaService
